eb2dcdfca52df06f7c3ff3b432051eea0d841797
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2023-03-14 16:57:05 +0100

    feat: ✨ add initial server config and development tools

---
+++ src/app/controllers/StatusGetController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { Controller } from './Controller';
+
+export default class StatusGetController implements Controller {
+  async run(_req: Request, res: Response) {
+    res.status(httpStatus.OK).send({ msg: 'OK' });
+  }
+}
